f95208dda30c540c07af4b399816f9770153b192
Dan Labrador <thonydaniellelabrador@gmail.com>
2024-04-04 08:34:37 -0400

    feat: include parcel package to enable using .env for local tests

--- js-api-app/scripts/util/openAIAPI.js
+++ js-api-app/scripts/util/openAIAPI.js
@@ -1,28 +1,63 @@
-import { OPENAI_KEY } from './apiKeys.js';
+import RetryUtil from './RetryUtil.js';
 
-const OpenAI = {
-  async getCompletion(prompt) {
-    try {
-      const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${OPENAI_KEY}`
-        },
-        body: JSON.stringify({
-          prompt: prompt,
-          max_tokens: 60
-        })
-      });
+class OpenAI {
+  async getCompletion(prompt, userId, instructions = 'You are a helpful assistant.', model = 'gpt-3.5-turbo') {
+    if (!prompt) throw new Error('Prompt is required.');
 
-      if (!response.ok) throw new Error('Request failed.');
+    const validModels = [
+      'gpt-4',
+      'gpt-4-turbo-preview',
+      'gpt-4-vision-preview',
+      'gpt-4-32k',
+      'gpt-3.5-turbo',
+      'gpt-3.5-turbo-16k'
+    ];
 
-      const json = await response.json();
+    const isModelValid = validModels.includes(model);
+    if (!isModelValid) throw new Error('Invalid model.');
 
-      return json.choices[0].text;
+    const messages = [
+      {
+        role: 'system',
+        content: instructions
+      },
+      {
+        role: 'user',
+        content: prompt
+      }
+    ];
 
-    } catch (e) {
-      console.log(e);
+    const temperature = 1.5;
+
+    const user = `aurorabeats/spotify:${userId}`;
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_KEY}`
+      },
+      body: JSON.stringify({ model, messages, temperature, user })
+    };
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+
+    const isRateLimited =
+      response.status === RetryUtil.TOO_MANY_REQUESTS ||
+      response.status === RetryUtil.SERVICE_UNAVAILABLE;
+
+    if (isRateLimited) {
+      const retryAfter = response.headers.get('Retry-After');
+      if (retryAfter) {
+        const delay = parseInt(retryAfter) * 1000;
+        return RetryUtil.retryAfterError(3, this.getCompletion(prompt), delay);
+      }
     }
+
+    const json = await response.json();
+    return json;
   }
+
 }
+
+export default OpenAI;
